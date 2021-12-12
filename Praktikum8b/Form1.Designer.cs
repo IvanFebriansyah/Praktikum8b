ad85f5a12b170a8e18b8afc9f8aee4e0b6184659
IvanFebriansyah <ivanfebriansyah@protonmail.com>
2021-12-13 01:39:43 +0700

    Add project files.

---
+++ Praktikum8b/Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿
+namespace Praktikum8b
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.txtfilter = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripButton6 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButton7 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton8 = new System.Windows.Forms.ToolStripButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.txtnama = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtharga = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.restoranDataSet = new Praktikum8b.restoranDataSet();
+            this.restoranDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dgvmenu = new System.Windows.Forms.DataGridView();
+            this.restoranDataSet1 = new Praktikum8b.restoranDataSet1();
+            this.menuBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.menuTableAdapter = new Praktikum8b.restoranDataSet1TableAdapters.menuTableAdapter();
+            this.idMenuDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.namaMenuDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.hargaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.restoranDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.restoranDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvmenu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.restoranDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.menuBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3,
+            this.toolStripButton4,
+            this.toolStripButton5,
+            this.toolStripSeparator1,
+            this.toolStripLabel1,
+            this.txtfilter,
+            this.toolStripButton6,
+            this.toolStripSeparator2,
+            this.toolStripButton7,
+            this.toolStripButton8});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(403, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "toolStripButton2";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton3.Text = "toolStripButton3";
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton4.Text = "toolStripButton4";
+            this.toolStripButton4.Click += new System.EventHandler(this.toolStripButton4_Click);
+            // 
+            // toolStripButton5
+            // 
+            this.toolStripButton5.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton5.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton5.Image")));
+            this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton5.Name = "toolStripButton5";
+            this.toolStripButton5.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton5.Text = "toolStripButton5";
+            this.toolStripButton5.Click += new System.EventHandler(this.toolStripButton5_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(48, 22);
+            this.toolStripLabel1.Text = "Search :";
+            // 
+            // txtfilter
+            // 
+            this.txtfilter.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtfilter.Name = "txtfilter";
+            this.txtfilter.Size = new System.Drawing.Size(140, 25);
+            // 
+            // toolStripButton6
+            // 
+            this.toolStripButton6.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton6.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton6.Image")));
+            this.toolStripButton6.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton6.Name = "toolStripButton6";
+            this.toolStripButton6.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton6.Text = "toolStripButton6";
+            this.toolStripButton6.Click += new System.EventHandler(this.toolStripButton6_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripButton7
+            // 
+            this.toolStripButton7.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton7.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton7.Image")));
+            this.toolStripButton7.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton7.Name = "toolStripButton7";
+            this.toolStripButton7.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton7.Text = "toolStripButton7";
+            this.toolStripButton7.Click += new System.EventHandler(this.toolStripButton7_Click);
+            // 
+            // toolStripButton8
+            // 
+            this.toolStripButton8.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton8.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton8.Image")));
+            this.toolStripButton8.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton8.Name = "toolStripButton8";
+            this.toolStripButton8.Size = new System.Drawing.Size(23, 20);
+            this.toolStripButton8.Text = "toolStripButton8";
+            this.toolStripButton8.Click += new System.EventHandler(this.toolStripButton8_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Id Menu";
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(140, 54);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(61, 20);
+            this.txtid.TabIndex = 2;
+            // 
+            // txtnama
+            // 
+            this.txtnama.Location = new System.Drawing.Point(140, 91);
+            this.txtnama.Name = "txtnama";
+            this.txtnama.Size = new System.Drawing.Size(141, 20);
+            this.txtnama.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(33, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nama Menu";
+            // 
+            // txtharga
+            // 
+            this.txtharga.Location = new System.Drawing.Point(140, 129);
+            this.txtharga.Name = "txtharga";
+            this.txtharga.Size = new System.Drawing.Size(141, 20);
+            this.txtharga.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(33, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Harga";
+            // 
+            // restoranDataSet
+            // 
+            this.restoranDataSet.DataSetName = "restoranDataSet";
+            this.restoranDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // restoranDataSetBindingSource
+            // 
+            this.restoranDataSetBindingSource.DataSource = this.restoranDataSet;
+            this.restoranDataSetBindingSource.Position = 0;
+            // 
+            // dgvmenu
+            // 
+            this.dgvmenu.AutoGenerateColumns = false;
+            this.dgvmenu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvmenu.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idMenuDataGridViewTextBoxColumn,
+            this.namaMenuDataGridViewTextBoxColumn,
+            this.hargaDataGridViewTextBoxColumn});
+            this.dgvmenu.DataSource = this.menuBindingSource;
+            this.dgvmenu.Location = new System.Drawing.Point(12, 171);
+            this.dgvmenu.Name = "dgvmenu";
+            this.dgvmenu.Size = new System.Drawing.Size(375, 277);
+            this.dgvmenu.TabIndex = 7;
+            // 
+            // restoranDataSet1
+            // 
+            this.restoranDataSet1.DataSetName = "restoranDataSet1";
+            this.restoranDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // menuBindingSource
+            // 
+            this.menuBindingSource.DataMember = "menu";
+            this.menuBindingSource.DataSource = this.restoranDataSet1;
+            // 
+            // menuTableAdapter
+            // 
+            this.menuTableAdapter.ClearBeforeFill = true;
+            // 
+            // idMenuDataGridViewTextBoxColumn
+            // 
+            this.idMenuDataGridViewTextBoxColumn.DataPropertyName = "idMenu";
+            this.idMenuDataGridViewTextBoxColumn.HeaderText = "idMenu";
+            this.idMenuDataGridViewTextBoxColumn.Name = "idMenuDataGridViewTextBoxColumn";
+            // 
+            // namaMenuDataGridViewTextBoxColumn
+            // 
+            this.namaMenuDataGridViewTextBoxColumn.DataPropertyName = "namaMenu";
+            this.namaMenuDataGridViewTextBoxColumn.HeaderText = "namaMenu";
+            this.namaMenuDataGridViewTextBoxColumn.Name = "namaMenuDataGridViewTextBoxColumn";
+            // 
+            // hargaDataGridViewTextBoxColumn
+            // 
+            this.hargaDataGridViewTextBoxColumn.DataPropertyName = "harga";
+            this.hargaDataGridViewTextBoxColumn.HeaderText = "harga";
+            this.hargaDataGridViewTextBoxColumn.Name = "hargaDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 460);
+            this.Controls.Add(this.dgvmenu);
+            this.Controls.Add(this.txtharga);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtnama);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toolStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CRUD B";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.restoranDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.restoranDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvmenu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.restoranDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.menuBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.ToolStripButton toolStripButton5;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox txtfilter;
+        private System.Windows.Forms.ToolStripButton toolStripButton6;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton toolStripButton7;
+        private System.Windows.Forms.ToolStripButton toolStripButton8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.TextBox txtnama;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtharga;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.BindingSource restoranDataSetBindingSource;
+        private restoranDataSet restoranDataSet;
+        private System.Windows.Forms.DataGridView dgvmenu;
+        private restoranDataSet1 restoranDataSet1;
+        private System.Windows.Forms.BindingSource menuBindingSource;
+        private restoranDataSet1TableAdapters.menuTableAdapter menuTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idMenuDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn namaMenuDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hargaDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ Praktikum8b/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Praktikum8b
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-ABD7H3A;Initial Catalog=restoran;Integrated Security=True");
+        DataTable dt = new DataTable();
+        BindingSource bS = new BindingSource();
+
+
+        private void resetdata()
+        {
+            txtid.Text = "";
+            txtnama.Text = "";
+            txtharga.Text = "";
+        }
+
+
+        private void bindingdata()
+        {
+            //clear data table
+            dt.Clear();
+
+            //clear databindings pada textbox
+            txtid.DataBindings.Clear();
+            txtnama.DataBindings.Clear();
+            txtharga.DataBindings.Clear();
+
+            //buat data adapter dan sqlbuilder
+            SqlDataAdapter da = new SqlDataAdapter("select * from menu", con);
+            SqlCommandBuilder scb = new SqlCommandBuilder(da);
+
+            //fill data ke dalam datatable
+            da.Fill(dt);
+
+            //set bindingsource datasource dan tampilkan ke dalam grid
+            bS.DataSource = dt;
+            dgvmenu.DataSource = bS;
+
+            //binding data ke dalam textbox yang ada
+            txtid.DataBindings.Add("Text", bS, "idMenu");
+            txtnama.DataBindings.Add("Text", bS, "namaMenu");
+            txtharga.DataBindings.Add("Text", bS, "harga");
+        }
+
+        private void showdata()
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from menu";
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(ds, "menu");
+            dgvmenu.DataSource = ds;
+            dgvmenu.DataMember = "menu";
+            dgvmenu.ReadOnly = true;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'restoranDataSet1.menu' table. You can move, or remove it, as needed.
+            showdata();
+            resetdata();
+            bindingdata();
+
+        }
+
+        private void toolStripButton6_Click(object sender, EventArgs e)
+        {
+            bS.Filter = "namaMenu like '%" + txtfilter.Text + "%'";
+        }
+
+        private void toolStripButton8_Click(object sender, EventArgs e)
+        {
+            bS.MoveNext();
+        }
+
+        private void toolStripButton7_Click(object sender, EventArgs e)
+        {
+            bS.MovePrevious();
+        }
+
+        private void toolStripButton5_Click(object sender, EventArgs e)
+        {
+            showdata();
+            resetdata();
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            resetdata();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            if(txtid.Text == "" | txtnama.Text == "" | txtharga.Text == "")
+            {
+                MessageBox.Show("Semua data menu harus diisi", "Peringatan");
+            }
+
+            int num;
+            bool isNum = int.TryParse(txtharga.Text.ToString(), out num);
+
+            if (!isNum)
+            {
+                MessageBox.Show("Isi harga harus angka", "Peringatan");
+                goto berhenti;
+            }
+
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "UPDATE menu SET namaMenu= '" + txtnama.Text + "',harga=" + int.Parse(txtharga.Text) + "'";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            showdata();
+            resetdata();
+
+        berhenti:
+
+            ;
+        }
+
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {
+            if(txtid.Text == "")
+            {
+                MessageBox.Show("Isi id menu yang akan dihapus");
+                goto berhenti;
+            }
+
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "delete from menu where idmenu = '" + txtid.Text + "'";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            showdata();
+            resetdata();
+
+        berhenti:
+            ;
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            if(txtid.Text == "" | txtnama.Text == "" | txtharga.Text == "")
+            {
+                MessageBox.Show("Semua data menu harus diisi", "Peringatan");
+                goto berhenti;
+            }
+
+            int num;
+            bool isNum = int.TryParse(txtharga.Text.ToString(), out num);
+
+            if (!isNum)
+            {
+                MessageBox.Show("Isi harus angka", "Peringatan");
+                goto berhenti;
+            }
+
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into menu values('" + txtid.Text + "','" + txtnama.Text + "'," + int.Parse(txtharga.Text) + ")";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            showdata();
+            resetdata();
+
+        berhenti:
+            ;
+        }
+    }
+}
